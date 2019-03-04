932e1b069e7daf2552a0144d19df6f3e50c2e1de
Ruslan89320 <gareevrusel.ru@yandex.ru>
2019-03-04 20:18:32 +0300

    Стартовая версия, тут тупо подключен движок и проект создан, больше ваще нихуя нет (сука смайлов нет)

---
+++ desktop/src/com/tamagochi/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tamagochi.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tamagochi.game.Tamagochi;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Tamagochi(), config);
+	}
+}
